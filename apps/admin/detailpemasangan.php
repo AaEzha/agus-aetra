e97731e0cda74ecff071335a83143a7403d21fe9
aa-azizah <aaezha@gmail.com>
2015-11-05 08:40:48 +0700

    update detail pemasangan

--- apps/admin/detailpemasangan.php
+++ apps/admin/detailpemasangan.php
@@ -7,7 +7,7 @@
         <li class="tab col s3"><a href="#test2">Detail Pemasang</a></li>
       </ul>
     </div>
-    <div id="test1" class="col s12">Test 1</div>
+    <div id="test1" class="col s12"></div>
     <div id="test2" class="col s12">Test 2</div>
   </div>
         
